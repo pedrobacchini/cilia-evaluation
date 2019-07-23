e55c0be95f53722a5b417fd8d446e40d261a1b49
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2019-07-23 19:37:07 -0300

    Initial Commit.

---
+++ src/main/java/com/github/pedrobacchini/ciliaevaluation/CiliaEvaluationApplication.java
@@ -0,0 +1,11 @@
+package com.github.pedrobacchini.ciliaevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiliaEvaluationApplication {
+
+    public static void main(String[] args) { SpringApplication.run(CiliaEvaluationApplication.class, args); }
+
+}
